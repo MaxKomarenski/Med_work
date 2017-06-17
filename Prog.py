fc98e528e059f5bc2409241e9b50ff4f54e2c945
LAPTOP-HFQ73LSO\HP <max99919991@gmail.com>
2017-06-17 13:51:16 +0300

    add

--- Prog.py
+++ Prog.py
@@ -1,25 +1,26 @@
-dict = {}
 def Input():
+    dict = {}
     while True:
         print("Enter - 1 if you want to input test question\n"
               "Enter - 2 if you want to input exact question\n"
               "Enter - 0 if you want to end")
         type_question = int(input("Enter number of type of question: "))
-        if type_question == 1: #test question
+        if type_question == 1:  # test question
             question = input("Enter the question -> ")
             lst_answer = []
+            print("Now you're writing 3 answers and only one of them is true.\n"
+                  "When you write a true answer, please put # before the answer.\n"
+                  "Example -> #People have two arms")
+            print("-----------------------")
             for i in range(3):
-                answer = input("Enter the answer. If answer is true, write before this '#'\n"
-                               "Example -> #How many arms people have ?")
+                answer = input("Enter the answer -> ")
                 lst_answer.append(answer)
             dict[question] = lst_answer
-            print(dict)
-        elif type_question == 2: #exact question
+        elif type_question == 2:  # exact question
             question = input("Enter the question -> ")
             answer = input("Enter the answer -> ")
             dict[question] = answer
-            print(dict)
-        else: #finishing of input
+        else:  # finishing of input
             break
 
     print('work')
