8fe9e6f432101ae87efad5f7187f8d3587adb0c1
LAPTOP-HFQ73LSO\HP <max99919991@gmail.com>
2017-06-19 16:46:28 +0300

    class

--- Prog.py
+++ Prog.py
@@ -1,58 +1,64 @@
 from tkinter import *
 import json
 
-root = Tk()
-but_type_test = Button(root,
-                       text="Test question",
-                       width=30, height=5,
-                       bg='white', fg='blue')
-but_type_exact = Button(root,
-                        text="Exact question",
-                        width=30, height=5,
-                        bg='white', fg='blue')
-but_finish = Button(root,
-                    text="End",
-                    width=30, height=5,
-                    bg='white', fg='blue')
 
-lab = Label(root,
-            text="Please, choose type of question",
-            font="Arial 12")
+class Med_prog():
+    def __init__(self):
+        self.main_dict = {}
+        self.but_type_test = Button(root,
+                               text="Test question",
+                               width=30, height=5,
+                               bg='white', fg='blue')
+        self.but_type_exact = Button(root,
+                                text="Exact question",
+                                width=30, height=5,
+                                bg='white', fg='blue')
+        self.but_finish = Button(root,
+                            text="End",
+                            width=30, height=5,
+                            bg='white', fg='blue')
 
-lab.pack()
+        self.lab = Label(root,
+                    text="Please, choose type of question",
+                    font="Arial 12")
 
-but_type_test.pack(side='left')
-but_finish.pack(side='right')
-but_type_exact.pack(side='bottom')
+        self.lab.pack()
 
-main_dict = {}
+        self.but_type_test.pack(side='left')
+        self.but_finish.pack(side='right')
+        self.but_type_exact.pack(side='bottom')
 
-def fun(event):
-    # test question
-    question = input("Enter the question -> ")
-    lst_answer = []
-    print("Now you're writing 3 answers and only one of them is true.\n"
+        self.but_type_test.bind("<Button-1>", self.test_question)
+        self.but_type_exact.bind("<Button-1>", self.exact_question)
+        self.but_finish.bind("<Button-1>", self.end)
+
+    def test_question(self, event):
+        # test question
+        window = Toplevel(root)
+        question = input("Enter the question -> ")
+        lst_answer = []
+        print("Now you're writing 3 answers and only one of them is true.\n"
               "When you write a true answer, please put # before the answer.\n"
               "Example -> #People have two arms")
-    print("-----------------------")
-    for i in range(3):
+        print("-----------------------")
+        for i in range(3):
+            answer = input("Enter the answer -> ")
+            lst_answer.append(answer)
+        self.main_dict[question] = lst_answer
+
+    def exact_question(self, event):
+        # exact question
+        question = input("Enter the question -> ")
         answer = input("Enter the answer -> ")
-        lst_answer.append(answer)
-    main_dict[question] = lst_answer
+        self.main_dict[question] = answer
 
-def bun(event):
-    # exact question
-    question = input("Enter the question -> ")
-    answer = input("Enter the answer -> ")
-    main_dict[question] = answer
+    def end(self, event):
+        print(self.main_dict)
+        json.dump(self.main_dict, open("test.json", "w"))
 
-def ret(event):
-    print(main_dict)
-    json.dump(main_dict, open("test.json", "w"))
 
-but_type_test.bind("<Button-1>", fun)
-but_type_exact.bind("<Button-1>", bun)
-but_finish.bind("<Button-1>", ret)
+root = Tk()
+root.title('Root')
+obj = Med_prog()
 
 root.mainloop()
-
